196c7568a41db1cfcb1121ba1a802f073f8e63f1
ibnu naufal <ibnunaufal5758@gmail.com>
2023-05-05 07:59:21 +0700

    initial

---
+++ old/credential.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBXeDqSJdyf__-B3btHS76SyWcOVjlcHAY",
+    authDomain: "notulen-app.firebaseapp.com",
+    projectId: "notulen-app",
+    storageBucket: "notulen-app.appspot.com",
+    messagingSenderId: "280223335705",
+    appId: "1:280223335705:web:30ded1cb43819af03b7188"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
