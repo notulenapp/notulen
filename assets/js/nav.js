8c6f8027b51da007b988dc647c9a88515baf5d14
ibnu naufal <ibnunaufal5758@gmail.com>
2023-05-23 20:35:07 +0700

    finishing touch

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -5,3 +5,12 @@ if (window.localStorage.getItem('role') != "ADMIN") {
     document.getElementById("nav-data-user").setAttribute('class', '')
     document.getElementById("nav-data-pegawai").setAttribute('class', '')
 }
+function recheckRole(){
+    if (window.localStorage.getItem('role') != "ADMIN") {
+        document.getElementById("nav-data-user").setAttribute('class', 'd-none')
+        document.getElementById("nav-data-pegawai").setAttribute('class', 'd-none')
+    } else {
+        document.getElementById("nav-data-user").setAttribute('class', '')
+        document.getElementById("nav-data-pegawai").setAttribute('class', '')
+    }
+}
